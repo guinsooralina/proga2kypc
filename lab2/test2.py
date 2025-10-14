0e5b110b6737ec724d18af911b553afa4345ad1d
guinsooralina <zatcepilamenya1203@gmail.com>
2025-10-14 10:13:00 +0300

    Update test2.py

--- lab2/test2.py
+++ lab2/test2.py
@@ -1,5 +1,5 @@
 import unittest
-
+from pr2 import twoSum
 # Функция, которую будем тестировать
 def add(a, b):
     return a + b
@@ -7,13 +7,13 @@ def add(a, b):
 # Тесты
 class TestMath(unittest.TestCase):
     def test_add_positive(self):
-        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(sorted(twoSum([2,7,6], 9)), [0,1])
 
     def test_add_negative(self):
-        self.assertEqual(add(-1, -3), -4)
+        self.assertEqual(sorted(twoSum([5, 4, 11, 8], 13)), [0, 3])
 
     def test_add_zero(self):
-        self.assertEqual(add(0, 5), 5)
+        self.assertEqual(sorted(twoSum([1, 10, 12, 9, 13], 14)), [0, 4])
 
 # Запуск тестов
 unittest.main(argv=[''], verbosity=2, exit=False)
